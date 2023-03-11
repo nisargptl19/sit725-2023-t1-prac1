a5eab418272b5f71c7bb643756fa38a7247a0170
nisargptl19 <127504380+nisargptl19@users.noreply.github.com>
2023-03-11 16:39:38 +1100

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+//variables to keep track 
+var previousIndex = -1;
+var counter = 0;
+
+function generateRandom(length) {
+  return Math.floor(Math.random() * length);
+}
+
+function updateText() {
+  // Array of text to display
+  var textArray = [
+    "Hello World!",
+    "Welcome to SIT725",
+    "This is javascript demo",
+    "Programming is beautiful",
+  ];
+  // Generate random index
+  var randomIndex = generateRandom(textArray.length);
+  console.log(randomIndex);
+
+  // Check if the random index is same as previous, if yes then displays text with count
+  if (randomIndex === previousIndex) {
+    counter++;
+    document.getElementById("heading").innerHTML =
+      textArray[randomIndex] + " (" + counter + ")";
+  } else {
+    counter = 0;
+    document.getElementById("heading").innerHTML = textArray[randomIndex];
+  }
+  previousIndex = randomIndex;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# sit725-2023-t1-prac1
+ SIT725 Practical 1 : Introduction of Programming. The code changes the displayed text as button is pressed.
